47ab50dbda5cd732eb33d17d598d3442d1064ba8
janis.pommers <jpommers@gmail.com>
2023-08-15 16:22:22 +0200

    added a happy path test

--- Trivia/Tests/GameTests.cs
+++ Trivia/Tests/GameTests.cs
@@ -1,11 +1,43 @@
-﻿namespace Tests;
+﻿using FluentAssertions;
+using System;
+using System.IO;
+using System.Xml.Linq;
+using Trivia;
+using Xunit;
+
+namespace Tests;
 
 public class GameTests
 {
+    private readonly StringWriter _stringWriter;
+    private readonly Game _game;
+
     public GameTests()
     {
+        _stringWriter= new StringWriter();
+        _game = new Game(_stringWriter);
 
+        _game.Add("Janis");
+        _game.Add("Peter");
     }
 
+    [Fact]
+    public void WhenAllAnswerCorrectly_FirstPlayerShouldWin()
+    {
+        //ARRANGE
+        var expectedOutput = File.ReadAllText($"ExpectedOutput/{nameof(WhenAllAnswerCorrectly_FirstPlayerShouldWin)}.txt");
 
+        //ACT
+        bool _notAWinner;
+        do
+        {
+            _game.Roll(1);
+
+            _notAWinner = _game.WasCorrectlyAnswered();
+
+        } while (_notAWinner);
+
+        //ASSERT
+        _stringWriter.ToString().Should().Be(expectedOutput);
+    }
 }
